b61f628e4bb628d32d866a4a96be9f7d22aee3c8
mathis sebille <mathis.sebille@affluences.com>
2025-11-27 11:49:00 +0100

    refactor: remove deprecated master token plugin and related components

--- apps/web/src/lib/auth/plugins/guards.ts
+++
@@ -1,1 +0,0 @@
-export * from './masterToken/guard'
--- apps/web/src/lib/auth/plugins/loginAs/index.ts
+++
@@ -1,9 +0,0 @@
-import { BetterAuthClientPlugin } from "better-auth"
-
-export const loginAsClientPlugin = () => {
-    return {
-        id: 'login-as',
-         
-        $InferServerPlugin: {} as ReturnType<typeof import('@repo/auth/server').loginAsPlugin>,
-    } satisfies BetterAuthClientPlugin
-}
